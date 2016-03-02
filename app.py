2ac2b2d49b7051c6ee5ee0949872626614ea1cea
Joseph D Rivera <rivera.d.joseph@gmail.com>
2016-03-02 10:57:35 -0500

    added a secret key for CRSR security

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from pokepong.app import app
 from pokepong.config import _cfg, _cfgi, _cfgb
 
 if __name__ == '__main__':
-    app.run(host=_cfg("debug-host"), port=_cfgi('debug-port'), debug=True)
+    app.secret_key = _cfg("secret-key")
+    app.run(host=_cfg("debug-host"),
+            port=_cfgi('debug-port'),
+            debug=True)
