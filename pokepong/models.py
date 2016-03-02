8a74a7252fbe8a8e35d6ba7a8af7188c75c8a925
Joseph D Rivera <rivera.d.joseph@gmail.com>
2016-03-02 10:58:55 -0500

    fixed some model typos

--- pokepong/models.py
+++ pokepong/models.py
@@ -7,12 +7,12 @@ import bcrypt
 app.config['SQLALCHEMY_DATABASE_URI'] = _cfg('connection-string')
 db = SQLAlchemy(app)
 
-class Trainer(db.model):
+class Trainer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, Nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String)
-    admin = Column(db.Boolean)
-    created = Column(db.DateTime)
+    admin = db.Column(db.Boolean)
+    created = db.Column(db.DateTime)
 
     def __init__(self, username, password, admin=False):
         self.username = username
@@ -24,10 +24,3 @@ class Trainer(db.model):
         self.password = bcrypt.hashpw(password.encode('utf-8'),
                                       bcrypt.gensalt()).decode('utf-8')
 
-class Pokemon(db.model):
-    #likely need at least a name, level and associated trainer
-    pass
-
-class Item(db.model):
-    #likely just link item id in complete item db to trainer(2 relationships)
-    pass
