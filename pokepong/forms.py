68c34624b0b0e038fda12be2db3de4cf3aa5bd44
Joseph D Rivera <rivera.d.joseph@gmail.com>
2016-03-14 17:08:37 -0400

    remove server form, convert pokemon select to checkboxes

--- pokepong/forms.py
+++ pokepong/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SelectField, SelectMultipleField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SelectField, SelectMultipleField, widgets
+from wtforms.validators import DataRequired, ValidationError
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 class Register(Form):
     username = StringField(u'username', validators=[DataRequired()])
@@ -15,11 +20,13 @@ class PartySignup(Form):
     teamname = StringField(u'Teamname', validators=[DataRequired()])
     player1 = StringField(u'Player 1', validators=[DataRequired()])
     player2 = StringField(u'Player 2', validators=[DataRequired()])
+    pokemon = MultiCheckboxField(u'pokemon', coerce=int)
 
 class BattleSignup(Form):
     #should add a validator to check no more than 6 pokemon are selected
-    pokemon = SelectMultipleField(u'pokemon', validators=[DataRequired()])
+    pokemon = MultiCheckboxField(u'pokemon', validators=[DataRequired()], coerce=int)
 
-class ServerManager(Form):
-    mode = SelectField(u'Mode', choices=[('battle', 'battle'), ('party', 'party')])
+    def validate_pokemon(form, field):
+        if len(field.data) != 6:
+            raise ValidationError('Choose 6 pokemon')
 
