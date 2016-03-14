aa5d5ca81399d474f3ff7e40632400d95e5fa896
Joseph D Rivera <rivera.d.joseph@gmail.com>
2016-03-14 17:11:16 -0400

    convert dos2unix, remove unused functions

--- pokepong/static/js/pics.js
+++ pokepong/static/js/pics.js
@@ -7,11 +7,11 @@ for (i = 1; i <= 150; i++){
 	newinput.id = i;
 	newlabel.className = 'pkmn';
 	newlabel.setAttribute("for", i);
-	var newimg = document.createElement('i')
+	var newimg = document.createElement('i');
 	newimg.className = "sprite sprite-" + i;
 	newimg.style = 'margin-left: 35px; margin-top: 35px';
 	newlabel.appendChild(newimg);
 	document.getElementById("pkmns").appendChild(newinput);
 	document.getElementById("pkmns").appendChild(newlabel);
 }
-checkboxlimit(document.forms.pkmns.pkmn,6)
+checkboxlimit(document.forms.pkmns.pkmn,6);
