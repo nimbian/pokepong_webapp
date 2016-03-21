bd31f9a9dc8350a9d1f8ef1e263c77446fe0cbc9
Joseph D Rivera <rivera.d.joseph@gmail.com>
2016-03-21 12:53:29 -0400

    validate the pokemon number, readd the server manager form

--- pokepong/forms.py
+++ pokepong/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SelectField, SelectMultipleField, widgets
+from wtforms import StringField, PasswordField, SelectField, SelectMultipleField, widgets, BooleanField
 from wtforms.validators import DataRequired, ValidationError
 
 
@@ -22,11 +22,19 @@ class PartySignup(Form):
     player2 = StringField(u'Player 2', validators=[DataRequired()])
     pokemon = MultiCheckboxField(u'pokemon', coerce=int)
 
+    def validate_pokemon(self, field):
+        if len(field.data) != 6:
+            raise ValidationError('Choose 6 pokemon')
+
 class BattleSignup(Form):
     #should add a validator to check no more than 6 pokemon are selected
     pokemon = MultiCheckboxField(u'pokemon', validators=[DataRequired()], coerce=int)
 
-    def validate_pokemon(form, field):
+    def validate_pokemon(self, field):
         if len(field.data) != 6:
             raise ValidationError('Choose 6 pokemon')
 
+class ServerManager(Form):
+    mode = SelectField(u'Game Mode', choices=[('party', 'party'),
+                                              ('battle', 'battle')])
+    purge = BooleanField(u'Purge')
