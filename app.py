ba1901745760a1f44ae7bbc954e64700f52c684e
Joseph D Rivera <rivera.d.joseph@gmail.com>
2016-04-07 16:12:39 -0400

    remove defaults as they are now part of config module

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from __future__ import absolute_import, print_function
 from pokepong.app import create_app
 
-
-DEBUG = True
-SECRET_KEY = 'development key'
-
 app = create_app()
 if __name__ == '__main__':
     app.run()
