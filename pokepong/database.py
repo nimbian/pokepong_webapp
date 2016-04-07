5ac17cf1b3d78640dd2da1e86ecba116bc0780da
Joseph D Rivera <rivera.d.joseph@gmail.com>
2016-04-07 16:14:53 -0400

    changed to use flask config and only run pokemon.sql when empty

--- pokepong/database.py
+++ pokepong/database.py
@@ -1,9 +1,10 @@
+from flask import current_app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from pokepong.config import _cfg
-engine = create_engine(_cfg('connection-string'))
+from flask import current_app
+engine = create_engine(current_app.config['CONNECTION_STRING'])
 db = scoped_session(sessionmaker(autocommit=False,
                                  autoflush=False,
                                  bind=engine))
@@ -15,6 +16,8 @@ def init_db():
     import pokepong.models
     Base.metadata.create_all(bind=engine)
     with engine.begin() as conn:
-        with open('pokepong/pokemon.sql', 'r') as file_:
-            for line in file_:
-                conn.execute(line)
+        result = conn.execute("SELECT id FROM pokemon")
+        if result.first() is None:
+            with open('pokepong/pokemon.sql', 'r') as file_:
+                for line in file_:
+                    conn.execute(line)
